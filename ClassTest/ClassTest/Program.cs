4c7ff8066d36f3e23f6a22b8c64a7dcc0c2cf3c1
HassanAhmed <HassanAhmed@DESKTOP-4JCP7PR>
2020-06-18 20:13:14 +0200

    Comitting

---
+++ ClassTest/ClassTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ClassTest/ClassTest/TestClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassTest
+{
+    class TestClass { }
+    {
+        public int Property1class { get; set; }
+        public string Proprty2 { get; set; }
+    }
+}
